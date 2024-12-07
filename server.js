fa21db7fc7462b63db60ad3578195ffe8fd8132f
dhanrajpimple <dhanrajpimple16012001@gmail.com>
2024-12-07 23:20:51 +0530

    ADD CI/CD workflow

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/",(req, res)=>{
     res.send("this is dhanraj testing ")
 })
 
-
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>{
     console.log("server is running")
 })
